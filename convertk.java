b8f90297fe0cb23737971ed0309ec48a85db8a77
keertanaganiga <43719847+keertanaganiga@users.noreply.github.com>
2020-12-08 16:28:00 +0530

    8_Dec_2020

---
+++ convertk.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class convertk
+{
+public static void main(String args[])throws Exception
+{
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+int n=Integer.parseInt(br.readLine());
+System.out.println(n);
+}
+}
+
