b4b78751cbd33d5beb9f0d1ee79c666d72fdc8b5
keertanaganiga <43719847+keertanaganiga@users.noreply.github.com>
2020-12-08 16:04:55 +0530

    8_Dec_2020_Java_Program

---
+++ convert.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class convert
+{
+public static void main(String args[])
+{
+char chart;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the character");
+chart=sc.next().charAt(0);
+int b=(int) chart;
+System.out.println(b);
+
+}
+}
