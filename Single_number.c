7eab76dcd31dd73a606764be508245b118772c3a
keertanaganiga <43719847+keertanaganiga@users.noreply.github.com>
2021-06-07 10:21:05 -0700

    Create Single_number.c

---
+++ Single_number.c
@@ -0,0 +1,28 @@
+/*
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+ 
+
+Example 1:
+
+Input: nums = [2,2,1]
+Output: 1
+Example 2:
+
+Input: nums = [4,1,2,1,2]
+Output: 4
+Example 3:
+
+Input: nums = [1]
+Output: 1
+*/
+int singleNumber(int* nums, int numsSize){
+        int i;
+    int result =0;
+    for(i=0;i<numsSize;++i)
+       result^=nums[i];
+    
+    return result;
+}
