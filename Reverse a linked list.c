54ed4ca3d3d025a84c5c4740b4373ac6beca6dae
keertanaganiga <43719847+keertanaganiga@users.noreply.github.com>
2021-06-12 12:00:48 -0700

    Create Reverse a linked list.c

---
+++ Reverse a linked list.c
@@ -0,0 +1,105 @@
+/*
+Given a linked list of N nodes. The task is to reverse this list.
+
+Example 1:
+
+Input:
+LinkedList: 1->2->3->4->5->6
+Output: 6 5 4 3 2 1
+Explanation: After reversing the list, 
+elements are 6->5->4->3->2->1.
+Example 2:
+
+Input:
+LinkedList: 2->7->8->9->10
+Output: 10 9 8 7 2
+Explanation: After reversing the list,
+elements are 10->9->8->7->2.
+
+*/
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+}*start;
+
+void insert();
+void display(struct Node *head);
+
+
+
+struct Node* reverseList(struct Node *head)
+    {
+        struct Node *prevnode,*currentnode,*nextnode;
+        prevnode=0;
+        currentnode=nextnode=head;
+        while(nextnode!=0)
+        {
+            nextnode=nextnode->next;
+            currentnode->next=prevnode;
+            prevnode=currentnode;
+            currentnode=nextnode;
+        }
+        head=prevnode;
+    }
+    
+
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+      start=NULL;
+      insert();
+      start = reverseList(start);
+      display(start);
+      printf("\n");
+    }
+    return 0;
+
+}
+
+
+ void insert()
+ {
+     int n,value,i;
+     scanf("%d",&n);
+     struct Node *temp;
+     for(i=0;i<n;i++)
+     {
+         scanf("%d",&value);
+         if(i==0)
+         {
+              start=(struct Node *) malloc( sizeof(struct Node) );
+              start->data=value;
+              start->next=NULL;
+              temp=start;
+              continue;
+         }
+         else
+         {
+             temp->next= (struct Node *) malloc( sizeof(struct Node) );
+             temp=temp->next;
+             temp->data=value;
+             temp->next=NULL;
+         }
+     }
+ }
+ 
+ void display(struct Node *head)
+{
+   while(head!=NULL)
+  {
+   printf("%d ",head->data);
+    head=head->next;
+  }
+}
+
+
+
